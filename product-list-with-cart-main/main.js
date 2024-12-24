e1570af51c686f828bef2c51a8306c121d21940b
Deeeeya <tankonated@gmail.com>
2024-12-24 01:05:49 -0500

    finished design, started implementing js to cart

---
+++ product-list-with-cart-main/main.js
@@ -0,0 +1,11 @@
+const desserts = [
+  { id: 1, name: "Waffle with Berries", price: 6.5 },
+  { id: 2, name: "Vanilla Bean Crème Brûlée", price: 7.0 },
+  { id: 3, name: "Macaron Mix of Five", price: 8.0 },
+  { id: 4, name: "Classic Tiramisu", price: 5.5 },
+  { id: 5, name: "Pistachio Baklava", price: 4.0 },
+  { id: 6, name: "Lemon Meringue Pie", price: 5.0 },
+  { id: 7, name: "Rev Velvet Cake", price: 4.5 },
+  { id: 8, name: "Salted Caramel Brownie", price: 5.5 },
+  { id: 9, name: "Vanilla Panna Cotta", price: 6.5 },
+];
