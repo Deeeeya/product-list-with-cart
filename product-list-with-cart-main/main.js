fd1dc61c8268f4a411a889743e3fc0ccbc7a7d21
Deeeeya <tankonated@gmail.com>
2024-12-31 01:02:23 -0500

    implemented the ability to increase and decrease the quantity of the item you are adding in cart

--- product-list-with-cart-main/main.js
+++ product-list-with-cart-main/main.js
@@ -3,6 +3,9 @@ const dessertItemsContainer = document.getElementById("products-items");
 const confirmOrderButton = document.querySelector(".confirmOrder");
 const deliveryType = document.querySelector(".disclaimer");
 const orderPrice = document.querySelector(".totalContainer");
+const emptyCake = document.querySelector(".emptyCake");
+const emptyDescription = document.querySelector(".emptyDescription");
+const originalCartButton = document.querySelector(".addCartButton");
 
 let cartItems = [];
 
@@ -96,12 +99,11 @@ const formatCurrency = (price) => {
   }).format(price ?? 0);
 };
 
-const dessertElements = desserts
-  .map((item) => {
-    const isItemInCart = cartItems.find((item) => item.id === id);
-    if (!!isItemInCart) {
-    }
-    return `
+function renderDessertItems() {
+  const dessertElements = desserts
+    .map((item) => {
+      const isItemInCart = cartItems.find((item) => item.id === id);
+      return `
         <div class="foodItem">
                 <div class="imagePlusCart" >
                   <img
@@ -115,14 +117,24 @@ const dessertElements = desserts
                         : ""
                     }"
                   />
-                  <button onclick="addProductToCard(${
-                    item.id
-                  })" class="addCartButton" id="addButton">
+                  ${
+                    isItemInCart
+                      ? `<div class="newAddCartButton">
+                    <button class="decreaseButton" onclick={decreaseItem(${item.id})} id=${item.id}>
+                      <img class="minusBtn" src="/product-list-with-cart-main/assets/images/icon-decrement-quantity.svg" alt="minus" />
+                    </button>
+                    <p class="quantityAmount">${item.quantity}</p>
+                    <button class="increaseButton" onclick={increaseItem(${item.id})} id=${item.id}>
+                      <img class="plusBtn" src="/product-list-with-cart-main/assets/images/icon-increment-quantity.svg" alt="plus" />
+                      </button>
+                  </div>`
+                      : `<button onclick="addProductToCard(${item.id})" class="addCartButton" id=${item.id}>
                     <img
                       src="/product-list-with-cart-main/assets/images/icon-add-to-cart.svg"
                       alt="cart"
                     />Add to Cart
-                  </button>
+                  </button>`
+                  }
                 </div>
                 <div class="foodInfo">
                   <p class="title">${item.title}</p>
@@ -131,15 +143,14 @@ const dessertElements = desserts
                 </div>
         </div>
         `;
-  })
-  .join("");
-
-dessertItemsContainer.innerHTML = dessertElements;
+    })
+    .join("");
+  dessertItemsContainer.innerHTML = dessertElements;
+}
 
 const addProductToCard = (id) => {
   const dessert = desserts.find((dessert) => dessert.id === id);
   const existingCartItem = cartItems.find((item) => item.id === id);
-
   if (!!existingCartItem) {
     cartItems = cartItems.map((item) =>
       item.id === id ? { ...item, quantity: item.quantity + 1 } : item
@@ -148,6 +159,7 @@ const addProductToCard = (id) => {
     cartItems.push({
       ...dessert,
       quantity: 1,
+      inCart: true,
     });
   }
 
@@ -198,12 +210,122 @@ function getTotal(cartItems) {
   totalPriceHTML.innerHTML = `${formatCurrency(totalPrice)}`;
 }
 
-function deleteItem(cartItems) {
+function deleteItem(id) {
   for (let i = 0; i < cartItems.length; i++) {
     if (cartItems[i].id === id) {
       cartItems[i].quantity = 1;
       cartItems.splice(i, 1);
     }
   }
+  const cartItemElements = cartItems
+    .map(
+      (item) =>
+        ` 
+      <div class="cartItem">
+        <div>
+          <p class="itemName">${item.name}</p> 
+          <p class="itemInfo">${
+            item.quantity
+          }x <span class="itemPrice">@ ${formatCurrency(
+          item.price
+        )}</span> <span class="totalPriceInfo">${formatCurrency(
+          item.price * item.quantity
+        )}</span></p> 
+        </div>
+        <button onclick={deleteItem(${item.id})} id=${
+          item.id
+        } class="removeItem"><img src="/product-list-with-cart-main/assets/images/icon-remove-item.svg" alt="x"</button>
+      </div>
+      <hr />
+      
+  `
+    )
+    .join("");
+  cartItemsContainer.innerHTML = cartItemElements;
+  getTotal(cartItems);
+
+  if (cartItems.length === 0) {
+    confirmOrderButton.style.display = "none";
+    deliveryType.style.display = "none";
+    orderPrice.style.display = "none";
+    cartItemsContainer.innerHTML = `
+        <img
+          class="emptyCake"
+          src="/product-list-with-cart-main/assets/images/illustration-empty-cart.svg"
+          alt="empty"
+        />
+        <p class="emptyDescription">Your added items will appear here</p>`;
+  }
+}
+
+function increaseItem(id) {
+  for (let i = 0; i < cartItems.length; i++) {
+    if (cartItems[i] && cartItems[i].id == id) {
+      cartItems[i].quantity += 1;
+    }
+    getTotal(cartItems);
+  }
+}
+
+function decreaseItem(id) {
+  for (let i = 0; i < cartItems.length; i++) {
+    if (cartItems[i] && cartItems[i].id > id) {
+      cartItems[i].quantity -= 1;
+    }
+  }
   getTotal(cartItems);
 }
+
+renderDessertItems();
+
+// function changeButtonAppearance(id) {
+//   if (cartItems[id].inCart) {
+//     originalCartButton.style.display = "none";
+//   }
+//   getTotal(cartItems);
+// }
+
+/*
+const dessertElements = desserts
+      .map((id) => {
+        const isItemInCart = cartItems.find((item) => item.id === id);
+        return `
+        <div class="foodItem">
+                <div class="imagePlusCart" >
+                  <img
+                    class="itemPic"
+                    id="itemPicID"
+                    src="${item.imgUrl}"
+                    alt="${item.name}"
+                    style="${
+                      !!isItemInCart
+                        ? "border: 1px solid hsl(14, 86%, 42%);"
+                        : ""
+                    }"
+                  />
+                  <div class="newAddCartButton">
+                    <button class="decreaseButton" onclick={decreaseItem(${
+                      item.id
+                    })} id=${item.id}>
+                      <img class="minusBtn" src="/product-list-with-cart-main/assets/images/icon-decrement-quantity.svg" alt="minus" />
+                    </button>
+                    <p class="quantityAmount">${item.quantity}</p>
+                    <button class="increaseButton" onclick={increaseItem(${
+                      item.id
+                    })} id=${item.id}>
+                      <img class="plusBtn" src="/product-list-with-cart-main/assets/images/icon-increment-quantity.svg" alt="plus" />
+                      </button>
+                  </div>
+                </div>
+                <div class="foodInfo">
+                  <p class="title">${item.title}</p>
+                  <p class="foodName">${item.name}</p>
+                  <p class="price">${formatCurrency(item.price)}</p>
+                </div>
+        </div>
+        `;
+      })
+      .join("");
+
+    dessertItemsContainer.innerHTML = dessertElements;
+*/
